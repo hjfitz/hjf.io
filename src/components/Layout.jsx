a63a3cb8681098b3bd723d481264c16c824c3df3
Harry Fitzgerald <harry.f@elanco.com>
2021-01-26 09:19:12 +0000

    added hacks to blog posts. will consider renaming to \posts'

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-import SEO from './SEO'
+import SEO from './SEO';
 
-import '../styles/main.scss'
-import {Link} from 'gatsby'
+import '../styles/main.scss';
+import { Link } from 'gatsby';
 
-const Layout = ({children}) => {
-	return (
-		<>
-			<SEO />
-			<div className="min-h-full border-black border-16">
-				<main className="container p-8 mx-auto">
-					<section className="pt-4 pb-8 text-center">
-						<header className="pb-8">hjf.io</header>
-						<nav className="text-4xl grid gap-4 sm:grid-cols-5">
-							<Link to="/">Home</Link>
-							<Link to="/about">About</Link>
-							<Link to="/blog">Blog</Link>
-							<Link to="/hacks">Hacks</Link>
-							<Link to="/uses">/uses</Link>
-						</nav>
-					</section>
-					{children}
-				</main>
-			</div>
-		</>
-	)
-}
+const Layout = ({ children }) => (
+	<>
+		<SEO />
+		<div className="min-h-full border-black border-16">
+			<main className="container p-8 mx-auto">
+				<section className="pt-4 pb-8 text-center">
+					<header className="pb-8">hjf.io</header>
+					<nav className="text-4xl grid gap-4 sm:grid-cols-4">
+						<Link to="/">Home</Link>
+						<Link to="/about">About</Link>
+						<Link to="/blog">Blog</Link>
+						<Link to="/uses">/uses</Link>
+  </nav>
+  </section>
+				{children}
+  </main>
+  </div>
+  </>
+);
 
-export default Layout
+export default Layout;
