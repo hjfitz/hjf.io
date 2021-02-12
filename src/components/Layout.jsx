8e67e30379b884d3b2e95a95e4ee5135248f4fb8
harry <hjfitz@protonmail.ch>
2021-02-12 19:35:52 +0000

    added pagination as a post

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 // todo: potentially move this to gatsby-browser.js
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Link} from 'gatsby'
 
 import SEO from './SEO'
@@ -7,19 +7,26 @@ import Nav from './Nav'
 
 import '../styles/main.scss'
 
-const Layout = ({ children }) => (
-	<>
-		<SEO />
-		<div className="min-h-full text-black bg-white border-black border-16 dark:bg-gray-900 dark:text-gray-50">
-			<main className="container p-8 mx-auto">
-				<section className="pt-4 pb-8 text-center">
-					<header className="pb-8"><Link to="/">hjf.io</Link></header>
-					<Nav />
-				</section>
-				{children}
-			</main>
-		</div>
-	</>
-);
+const Layout = ({ children }) => {
+	useEffect(() => {
+		console.log('%c hjf.io ', 'background: #222; color: #fff; font-size: 32px')
+		console.log('Poking about? Check my code on GitHub!')
+		console.log('%c https://github.com/hjfitz?tab=repositories', 'color: #1e3a8a')
+	}, [])
+	return (
+		<>
+			<SEO />
+			<div className="min-h-full text-black bg-white border-black border-16 dark:bg-gray-900 dark:text-gray-50">
+				<main className="container p-8 mx-auto">
+					<section className="pt-4 pb-8 text-center">
+						<header className="pb-8"><Link to="/">hjf.io</Link></header>
+						<Nav />
+					</section>
+					{children}
+				</main>
+			</div>
+		</>
+	);
+}
 
 export default Layout;
