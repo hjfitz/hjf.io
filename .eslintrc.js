c898096105f3ae13a387b67eb9a7f5736fe83712
harry <hjfitz@protonmail.ch>
2021-08-02 06:39:01 +0100

    fix(lint): remediate conflicting eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-	  "indent": ["error", "tab"]
-  },
+	env: {
+		browser: true,
+		es2020: true,
+		node: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: [
+		'react',
+	],
+	rules: {
+		indent: ['error', 'tab'],
+		'no-tabs': 0,
+	},
 };
