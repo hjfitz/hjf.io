5f43216d0bc7b14abd4b7e8f7e05fa715e3af1b5
Harry Fitzgerald <harry.fitzgerald@truepill.com>
2021-12-20 16:51:58 +0000

    ui(footer): swap margin to padding in order to fill footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Footer = () => (
-	<footer className="flex mt-12 mb-4 text-sm text-gray-300 justify-evenly">
+	<footer className="flex mt-12 pb-4 text-sm text-gray-300 justify-evenly">
 		<a href="https://github.com/hjfitz/">git</a>
 		<a href="/rss.xml">feed</a>
 	</footer>
